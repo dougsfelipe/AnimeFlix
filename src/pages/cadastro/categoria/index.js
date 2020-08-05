5a40815a501325d4ad6ce827188212f81d620daf
dougsfelipe <douglassantos@viitra.com>
2020-08-05 01:12:31 -0300

    aula 4

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,19 +1,113 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria(){
-    return (
-        <PageDefault>
-        
-            <h1>Página de Cadastro de Categoria</h1>
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
 
-            <Link to="/cadastro/categoria">
-                Cadasrtar Categoria
-            </Link>
-        
-        </PageDefault>
-    )
+  const [categorias, setCategorias] = useState(['']);
+  const [values, setValues] = useState([valoresIniciais]);
+
+  function setValue(chave, valor) {
+    // chave: nome, descricao
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infoEvento) {
+    setValue(infoEvento.target.getAttribute('name'), infoEvento.target.value);
+  }
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
+  return (
+    <PageDefault>
+
+      <h1>
+        Página de Cadastro de Categoria
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosEvento) {
+        infosEvento.preventDefault();
+
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <div>
+
+          <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <FormField
+            label="Descrição"
+            type="textarea"
+            name="descricao"
+            value={values.descricao}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
+        </div>
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => (
+        // eslint-disable-next-line react/no-array-index-key
+          <li key={`${categoria.nome}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+      <Link to="/">
+        Ir para home
+      </Link>
+
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
